2897843bac86ae4d7c51fc70113cc79e133fecee
Miguel Castaneda <miguelcastaneda@Miguels-MBP-3.lan>
2022-09-27 21:32:05 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_203113) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_041129) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_203113) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
